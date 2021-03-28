ab43f6a59d651f6a72714df9eb767ae6bb4e8f34
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-28 12:32:22 +0900

    Greet known user

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,30 @@
 package example
 
 object Hello extends Greeting with App {
-  println(greeting)
+  List(1, 2, 3)
+    .map(greetUser(_))
+    .foreach {
+      case Left(error) => println(error)
+      case Right(Some(name)) => println(name)
+      case Right(None) => println("John Does?")
+    }
 }
 
 trait Greeting {
-  lazy val greeting: String = "hello"
+  def getUser(id: Long): Either[String, User] =
+    id match {
+      case 1 => Right(User(1, Option("Mokele-mbembe")))
+      case 2 => Right(User(2, None))
+      case _ => Left("Who are you?")
+    }
+
+  def greetUser(id: Long): Either[String, Option[String]] =
+    for {
+      user <- getUser(id)
+    } yield for {
+      name <- user.name
+    } yield s"Hello! $name."
 }
+
+case class User(id: Long, name: Option[String])
+
